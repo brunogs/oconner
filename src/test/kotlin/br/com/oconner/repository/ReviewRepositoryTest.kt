42cc1d2fd96e1918f7ae1fd85aaf109a1c2751f9
Bruno Silva <bruno.gomes@belezanaweb.com.br>
2021-10-08 01:57:23 -0300

    refactor: simplified ReviewRepositoryTest.kt

--- src/test/kotlin/br/com/oconner/repository/ReviewRepositoryTest.kt
+++ src/test/kotlin/br/com/oconner/repository/ReviewRepositoryTest.kt
@@ -2,6 +2,9 @@ package br.com.oconner.repository
 
 import br.com.oconner.domain.Customer
 import br.com.oconner.domain.Review
+import org.apache.commons.lang3.RandomStringUtils
+import org.apache.commons.lang3.RandomStringUtils.randomAlphabetic
+import org.apache.commons.lang3.RandomUtils
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
@@ -20,11 +23,7 @@ class ReviewRepositoryTest(@Autowired val reviewRepository: ReviewRepository) {
 
     @Test
     fun `should create review`() {
-        val review = Review(
-            rating = 5,
-            author = Customer(name = "User Test"),
-            movieId = "123"
-        )
+        val review = createReview()
 
         val result = reviewRepository.save(review)
 
@@ -34,21 +33,24 @@ class ReviewRepositoryTest(@Autowired val reviewRepository: ReviewRepository) {
 
     @Test
     fun `should not allow duplicated review`() {
-        val reviewOne = Review(
-            rating = 5,
-            author = Customer(name = "User Test"),
-            movieId = "123"
-        )
-
-        val reviewTwo = Review(
-            rating = 1,
-            author = Customer(name = "User Test"),
-            movieId = "123"
-        )
+        val reviewOne = createReview(customerName = "User Test", movieId = "123")
+        val reviewTwo = createReview(customerName = "User Test", movieId = "123")
 
         reviewRepository.save(reviewOne)
         assertThrows<DuplicateKeyException> {
             reviewRepository.save(reviewTwo)
         }
     }
+
+    private fun createReview(
+            customerName: String = randomAlphabetic(5),
+            rating: Int = RandomUtils.nextInt(),
+            movieId: String = randomAlphabetic(5)) : Review {
+        return Review(
+                rating = rating,
+                author = Customer(name = customerName),
+                movieId = movieId
+        )
+    }
+
 }
